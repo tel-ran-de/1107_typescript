4f0c0d60ff1851da017b85169d71658cd6cf6ae8
callbackKid <v.yarosh@ideco.ru>
2024-03-10 17:20:09 +0400

    ts

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log(1+'1')
+var srt1 = "1";
+var bool = true;
+var undef = undefined;
+var nullish = null;
+// создать перменную useName тип строка 
+// переменную userAge тип возраст
+// married булевое значение
+var userName = 'Vlad';
+var userAge = 30;
+var isMarried = false;
+// const smth: any = null
+// ИЗБЕГАТЬ ТИП ANY 
+// всегда заменять any на более корректный тип данных
+var sum = function (a, b) { return a + b; };
+sum(1, 2);
+// создать функцию кот. принимает параметры пользователя и возвращ  одну строку со всеми его данными
+var allUserData = function (name, age, married) {
+    return "My name is ".concat(name, " I am ").concat(age, " I am ").concat(married ? '' : 'not', " married");
+};
+allUserData(userName, userAge, isMarried);
+var logAllUserData = function (name, age, married) {
+    console.log("My name is ".concat(name, " I am ").concat(age, " I am ").concat(married ? '' : 'not', " married"));
+};
+// void -->  для функций кот вовзращают undefined
+logAllUserData(userName, userAge, isMarried);
+var arr = [1, 2, 3];
+var arrOfStrings = ['1', '2', '3'];
+var arrOfArray = [[1, 2, 3], [1, 2, 4]];
+var userData = {
+    userName: 'Vlad',
+    userAge: 30,
+    isMarried: false
+};
+var showDataFromObj = function (userObj) {
+    console.log(userObj.userName, userObj.userAge, userObj.isMarried);
+};
+var showDataFromDestrObject = function (_a) {
+    var userName = _a.userName, userAge = _a.userAge, isMarried = _a.isMarried;
+    console.log(userName, userAge, isMarried);
+};
+console.log('a');
+console.log('b');
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-const num: number = 1
-const srt: string = "1"
+const srt1: string = "1"
+const id: number = 1
 const bool: boolean = true
 const undef: undefined = undefined
 const nullish: null = null
@@ -28,6 +28,7 @@ allUserData(userName, userAge, isMarried)
 const logAllUserData = (name: string, age: number, married:boolean): void => {
     console.log(`My name is ${name} I am ${age} I am ${married ? '' : 'not' } married`)
 }
+// void -пустота
 // void -->  для функций кот вовзращают undefined
 logAllUserData(userName, userAge, isMarried)
 
@@ -43,10 +44,71 @@ const userData: {userName: string, userAge: number, isMarried: boolean} = {
     isMarried: false
 }
 
-const showDataFromObj = (userObj: {userName: string, userAge: number, isMarried: boolean}) => {
+const showDataFromObj = (userObj: {userName: string, userAge: number, isMarried: boolean}): void => {
         console.log(userObj.userName, userObj.userAge, userObj.isMarried)
 }
 
-const showDataFromDestrObject = ({userName, userAge, isMarried}: {userName: string, userAge: number, isMarried: boolean}) => {
+const showDataFromDestrObject = ({userName, userAge, isMarried}: {userName: string, userAge: number, isMarried: boolean}):void => {
         console.log(userName, userAge, isMarried)
+}
+
+
+const arrOfSmth = ["1", '2']
+// arr массив строк
+const toNumbers = (arr: string[]): number[] => {
+return arr.map((el: string ) => Number(el))
+}
+
+const check = toNumbers(arrOfSmth)
+
+console.log(check)
+
+
+// TUPLES - что-то среднее между массивом и объектом
+// {name: 'Ivan', age: 29}
+const userDataTuple: [string, string, number] = ['Ivan', 'berlin', 29]
+const data: [string, number, boolean] = ['Hello', 123, true];
+const coordinates: [number, number] = [0.5, 0.7]
+
+// первые два значения это числа а потом какое-то(не знаю точно) будет количество строк
+const listOfProducts: [number, number, ...string[]] = [550, 10, 'apple', 'banana', 'orange']
+
+
+const price: number | string = "100" // я объединяю два типа 
+//  либо придет массив строк либо придет undefined т.е ничего
+const images: string[] | undefined = ['htts://placeholde.img']
+// пока нету данных у меня явно стоит null 
+// но если какие-то данные придут то это будет число
+const result: number | null = null
+
+
+const urlCreator = (protocol: string, port: number | string): string => `${protocol}/127.0.0.1/${port}` 
+
+urlCreator('https', 3000)
+urlCreator('https', '3001')
+
+// написать функцию modify 
+// может принимать либо число либо строку 
+// если тип строка то мы возвращ массив из символов строки
+// а если число то возвращаем умнож число на два
+const modify = (data: string | number): string[] | number =>  typeof data ===  'string' ? [...data] : data *=2
+
+// Literal type = буквальный
+// БУКВАЛЬНО ГОВОРЮ ЧЕМ МОЖЕТ БЫТЬ МОЯ ПРЕМЕННАЯ
+const serverPort: 3000 | 3001 = 3001
+
+const responseStatus: 'isLoading' | 'error' | "successful" = 'error'
+
+type Colors = 'grey' | 'red' | 'green' // Literal type 
+type Status = 'isLoading' | 'error' | "successful" // Literal type 
+
+const returnColor = (status:  Status): Colors=> {
+    switch (status ) {
+        case 'isLoading':
+            return 'grey'
+        case 'error':
+            return 'red'
+        case 'successful':
+            return 'green'
+    }
 }
